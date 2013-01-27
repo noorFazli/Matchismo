6880f4b7b4e51d4f8023dcd2dad0dafdbbfd0c9e
Manuel Lorenzo <manulorenzop@gmail.com>
2013-01-27 13:05:18 +0100

    added PlayingCardDeck

---
+++ Matchismo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matchismo
+//
+//  Created by manu on 27/01/2013.
+//  Copyright (c) 2013 Manuel. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
