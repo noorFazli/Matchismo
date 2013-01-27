d9d3e3396d37c62ad82890c5ecaa6a0442a8d30d
Manuel Lorenzo <manulorenzop@gmail.com>
2013-01-27 11:46:21 +0100

    completed Card model implementation

--- Matchismo/Model/Card.h
+++ Matchismo/Model/Card.h
@@ -10,4 +10,9 @@
 
 @interface Card : NSObject
 
+@property (strong, nonatomic) NSString *contents;
+@property (nonatomic, getter=isFaceUp) BOOL faceUp;
+@property (nonatomic, getter=isUnPlayable) BOOL unPlayable;
+
+-(int)match:(NSArray *) otherCards;
 @end
